45d5f35ea4261c3106db28c0dc2499015941a2a0
Tomás Guinzburg <tomasguinzburg@gmail.com>
2020-12-01 21:41:44 -0300

    selects

--- src/components/last-order/md-circuits-jobs/ParseCircuitJob.ts
+++ src/components/last-order/md-circuits-jobs/ParseCircuitJob.ts
@@ -2,10 +2,10 @@ import { CircuitJobFormValues } from "./CircuitJobFormValues";
 
 export const createFormatedQuery = (form: CircuitJobFormValues): string => {
   //TODO
-  return `TODO`;
+  return `--TODO`;
 }
 
 export const createUnformatedQuery = (form: CircuitJobFormValues): string => {
   //TODO
-  return `TODO`;
+  return `--TODO`;
 }
