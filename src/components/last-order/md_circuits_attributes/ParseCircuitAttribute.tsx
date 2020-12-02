8e1d508c2933627667fabdd7474ac039540476c3
Franco Benchoa <franco.benchoa@gmail.com>
2020-12-01 22:23:52 -0300

    circuitattribute

---
+++ src/components/last-order/md_circuits_attributes/ParseCircuitAttribute.tsx
@@ -0,0 +1,11 @@
+import { CircuitAttributeFormValues } from "./CircuitAttributeFormValues";
+
+export const createFormatedQuery = (form: CircuitAttributeFormValues): string => {
+  //TODO
+  return `--TODO`;
+}
+
+export const createUnformatedQuery = (form: CircuitAttributeFormValues): string => {
+  //TODO
+  return `--TODO`;
+}
